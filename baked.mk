f3bb469a3dcd1887d41ce7cbdd62129e9a850da5
baked <travp624@gmail.com>
2014-01-01 13:42:46 -0600

    Setup ls980(g2-sprint) for baked building.
    Signed-off-by: baked <travp624@gmail.com>

--- baked.mk
+++ baked.mk
@@ -1,22 +1,22 @@
-## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/cdma.mk)
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/baked/config/cdma.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common baked stuff.
+$(call inherit-product, vendor/baked/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/baked/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/ls980/ls980.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ls980
-PRODUCT_NAME := cm_ls980
+PRODUCT_NAME := baked_ls980
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-LS980
 PRODUCT_MANUFACTURER := lge
